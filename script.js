e27cb358a2cd41dc1e9aaadbd48a76f1abbc616d
Gabriel Souza <gterhorsts@gmail.com>
2018-09-17 12:49:03 -0300

    Atualizacao

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+let db = connect('https://codes-fe0a1.firebaseio.com/')
+
+
+let path = '/design/courses/codes'
+
+db.download(path, function(data) {
+	
+
+
+
+
+	})
+
+}
