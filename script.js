e51be9014b9bc6962842ec7b4ba9eed4ce643420
Gabriel Souza <gterhorsts@gmail.com>
2018-09-18 12:59:54 -0300

    alteracoes

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
 let db = connect('https://co-design-9ee8e.firebaseio.com/')
 
 
-let path = '/design/courses/codes'
+let params = extract()
+
+let path = '/' + params['category'] + '/courses/' + params['course']
 
 db.download(path, function(data) {
 	
+	replace('body', {
+		'name': data['name'],
+		'goals': data['goals'],
+	})
 
 
 
 
 	})
 
-}https://codes-fe0a1.firebaseio.com/
+}
