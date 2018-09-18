a6bed5df091cc4296e8bb02876c51de91fb3fd3a
tluigimm <tluigimm@gmail.com>
2018-09-18 17:03:47 -0300

    json adicionado

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+	let db = connect('https://co-design-9ee8e.firebaseio.com/')
 
+	let params = extract()
 
-let db = connect('https://co-design-9ee8e.firebaseio.com/')
+	let url = '/' + params['category'] + '/courses/' + params['course']
 
-
-let params = extract()
-
-let path = '/' + params['category'] + '/courses/' + params['course']
-
-db.download(path, function(data) {
-	
-	replace('body', {
+	db.download(url, function(data) {
+		replace('body', {
 		'name': data['name'],
 		'goals': data['goals'],
+		})
 	})
-
-
-
-
-	})
-
-}
+})
